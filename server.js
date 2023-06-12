67daa674d3f0d555f8da035e6c8874a09c741f40
Jared Silver <jrsilver88@gmail.com>
2023-06-12 11:46:25 -0700

    set up basic routes and controllers for auth and jobs

--- server.js
+++ server.js
@@ -3,12 +3,19 @@ import notFoundMiddleware from './middleware/not-found.js'
 import errorHandlerMiddleware from './middleware/error-handler.js'
 import dotenv from 'dotenv'
 import connectDB from './db/connect.js'
+import authRouter from './routes/authRoutes.js'
+import jobsRouter from './routes/jobRoutes.js'
 dotenv.config()
 const app = express()
+
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('Welcome to my server!')
 })
+
+app.use('/api/v1/auth', authRouter)
+app.use('/api/v1/jobs', jobsRouter)
 
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
