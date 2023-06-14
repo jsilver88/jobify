9c71d7ed7799b200b2e8e6b07d078310cec7f17d
Jared Silver <jrsilver88@gmail.com>
2023-06-13 23:08:10 -0700

    added error handling for bad request, not found, and custom api errors

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,12 +1,25 @@
 import { StatusCodes } from 'http-status-codes'
 
 const errorHandlerMiddleware = (error, req, res, next) => {
-  console.log(error)
   const defaultError = {
-    statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
-    msg: 'Something went wrong, try again later',
+    statusCode: error.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: error.message || 'Something went wrong, try again later',
   }
-  res.status(defaultError.statusCode).json({ msg: error })
+
+  if (error.name === 'ValidationError') {
+    defaultError.statusCode = StatusCodes.BAD_REQUEST
+    // defaultError.msg = error.message
+    defaultError.msg = Object.values(error.errors)
+      .map((item) => item.message)
+      .join(',')
+  }
+
+  if (error.code && error.code === 11000) {
+    defaultError.statusCode = StatusCodes.BAD_REQUEST
+    defaultError.msg = `${Object.keys(error.keyValue)} field has to be unique`
+  }
+  // res.status(defaultError.statusCode).json({ msg: error })
+  res.status(defaultError.statusCode).json({ msg: defaultError.msg })
 }
 
 export default errorHandlerMiddleware
