9b950bca48a28b8590ebc5f034bcededc81a6ca9
Jared Silver <jrsilver88@gmail.com>
2023-06-16 17:02:44 -0700

    login functionality added to reactcontext and logging in

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -14,7 +14,7 @@ const initialState = {
 const Register = () => {
   const [values, setValues] = useState(initialState)
   const navigate = useNavigate()
-  const { user, isLoading, showAlert, displayAlert, registerUser } =
+  const { user, isLoading, showAlert, displayAlert, registerUser, loginUser } =
     useAppContext()
 
   const toggleMember = () => {
@@ -35,7 +35,7 @@ const Register = () => {
     }
     const currentUser = { name, email, password }
     if (isMember) {
-      console.log('already a member')
+      loginUser(currentUser)
     } else {
       registerUser(currentUser)
     }
